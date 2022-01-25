5bcf182943cc20be6e3b059ea26ca1b04407f4f4
Jon <47305276+Captain-Seli@users.noreply.github.com>
2022-01-25 09:05:59 -0500

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,33 @@
+`use strict`
+
+// Player Class
+// Target Class
+// Function to draw circles -> CSS/HTML
+// Movement Section
+// Game Board Section
+// Function for Difficulty Selection
+// Title and Player inputs
+// Display Current Player Name and Score
+// Return Player Score
+// Save Player Data
+// Main Gameplay Loop
+// Close Game Function
+
+
+/*const id = setInterval(frame, 5);
+document.querySelector(`.start`).addEventListener(`click`, function() {
+  const elem = document.getElementById(`ball`);
+  const pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + `px`;
+      elem.style.left = pos + `px`;
+    }
+  }
+})
+*/
